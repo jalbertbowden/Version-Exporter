28c74150c25e2cb639a0b20965e985cdfe2d507a
Artem Matevosyan <artem@mtvsn.com>
2013-11-04 12:05:29 +0100

    Fixed an error for cropping and trimming at the same time

--- include/trimmer.js
+++ include/trimmer.js
@@ -1,15 +1,23 @@
 
 function trimmer(docRef){
 
-    // Select All Layers
-    var desc1 = new ActionDescriptor();
-    var ref1 = new ActionReference();
-    ref1.putEnumerated(cTID('Lyr '), cTID('Ordn'), cTID('Trgt'));
-    desc1.putReference(cTID('null'), ref1);
-    executeAction(sTID('selectAllLayers'), desc1, DialogModes.NO);
+    // Get the number of layers in the document
+    var layersNumber = docRef.layers.length;
 
-    // Merge Layers
-    executeAction(sTID('mergeLayersNew'), undefined, DialogModes.NO);
+    // Merge layers if there are more than 1 layer
+    if (layersNumber > 1) {
+
+        // Select All Layers
+        var desc1 = new ActionDescriptor();
+        var ref1 = new ActionReference();
+        ref1.putEnumerated(cTID('Lyr '), cTID('Ordn'), cTID('Trgt'));
+        desc1.putReference(cTID('null'), ref1);
+        executeAction(sTID('selectAllLayers'), desc1, DialogModes.NO);
+
+        // Merge Layers
+        executeAction(sTID('mergeLayersNew'), undefined, DialogModes.NO);
+
+    }
 
     // Trim document
     docRef.trim(TrimType.TRANSPARENT);
